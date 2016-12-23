06737b84d5e4068bff494a92cd6e7b4e402a9f81
Daniel Stahl <danstahl1138@gmail.com>
2016-12-23 09:39:39 -0600

    M_PI compatibility

--- fft.h
+++ fft.h
@@ -5,6 +5,9 @@
 #include <vector>
 #define _USE_MATH_DEFINES
 #include <cmath>
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
 
 typedef std::complex<double> Complex;
 typedef std::vector<Complex> CArray;
