7f94fcfd17ae4e6ba4975343a43841b58f690f2d
Daniel Stahl <danstahl1138@gmail.com>
2016-12-24 16:43:08 -0600

    working fang oost

--- OptionPricing.h
+++ OptionPricing.h
@@ -8,30 +8,78 @@
 #include "RungeKutta.h"
 #include "fft.h"
 namespace optionprice{
+    /**
+    Used by FSTS method
+    */
     template<typename Index, typename Number>
     auto computeDX(const Index& xDiscrete, const Number& xMin,const Number& xMax){
         return (xMax-xMin)/(xDiscrete-1.0);
     }
+    /**
+    Used by FSTS method
+    */
     template<typename Index, typename Number>
     auto getDomain(const Number& xMin, const Number& dx, const Index& index){
         return xMin+index*dx;
     }
+
+    /**
+    Used by FSTS method
+    */
     template<typename Index, typename Number>
     auto getUDomain(const Number& uMax, const Number& du, const Index& index){
         auto myU=du*index;
         return myU>uMax?myU-2.0*uMax:myU;
     }
-    template<typename Number1, typename Number2>
-    auto getRealDomain(const Number1& startPoint, const Number2& logDomain){
-        return startPoint*exp(logDomain);
+
+    /**
+    Used by Carr Madan
+    */
+    template<typename Number>
+    auto getMaxK(const Number& ada){
+        return M_PI/ada;
+    }
+    template<typename Index, typename Number>
+    auto getLambda(const Index& numSteps, const Number& b){
+        return 2.0*b/numSteps;
     }
 
     template<typename Index, typename Number>
+    auto getFSTSUnderlying(const Number& xMin, const Number& xMax, const Number& K, const Index& numX){
+        auto dx=computeDX(numX, xMin, xMax);
+        return futilities::for_each_parallel(0, numX, [&](const auto& index){
+            return K*exp(getDomain(xMin, dx, index));
+        });
+    }
+
+    template<typename Index, typename Number>
+    auto getFangOostUnderlying(const Number& xMin, const Number& xMax, const Number& K, const Index& numX){
+        auto dx=fangoost::computeDX(numX, xMin, xMax);
+        return futilities::for_each_parallel(0, numX, [&](const auto& index){
+            return K*exp(fangoost::getX(xMin, dx, index));
+        });
+    }
+
+    template<typename Index, typename Number>
+    auto getCarrMadanStrikes(const Number& ada, const Number& S0, const Index& numX){
+        auto b=getMaxK(ada);
+        auto lambda=getLambda(numX, b);
+        return futilities::for_each_parallel(0, numX, [&](const auto& index){
+            return S0*exp(optionprice::getDomain(-b, lambda, index));
+        });
+    }
+
+    /*template<typename Number1, typename Number2>
+    auto getRealDomain(const Number1& startPoint, const Number2& logDomain){
+        return startPoint*exp(logDomain);
+    }*/
+
+    /*template<typename Index, typename Number>
     auto computeXRange(const Index& xDiscrete, const Number& xMin, const Number& xMax){
         return futilities::for_emplace_back(xMin, xMax, xDiscrete, [](const auto& val){
             return val;
         });
-    }
+    }*/
 
 
 
@@ -74,62 +122,54 @@ namespace optionprice{
     Fang Oosterlee approach
     
     */
-    template<typename Number, typename Index>
-    auto chiK(const Number& a, const Number& b, const Number& c, const Number& d, const Index& k){
-        auto efficientSquare=[](const auto& num){
+    template<typename Number, typename U>
+    auto chiK(const Number& a, const Number& b, const Number& c, const Number& d, const U& u){
+       /* auto efficientSquare=[](const auto& num){
             return num*num;
-        };
-        auto kPiBA=k*M_PI/(b-a);
+        };*/
+        //auto u=k*M_PI/(b-a);
         auto iterS=[&](const auto& x){
-            return kPiBA*(x-a);
+            return u*(x-a);
         };
-        auto coef=1.0/(1+efficientSquare(kPiBA));
+        //auto coef=1.0/;
         auto expD=exp(d);
         auto expC=exp(c);
-        return coef*(cos(iterS(d))*expD-cos(iterS(c))*expC+kPiBA*sin(iterS(d))*expD-kPiBA*sin(iterS(c))*expC);
+        return (cos(iterS(d))*expD-cos(iterS(c))*expC+u*sin(iterS(d))*expD-u*sin(iterS(c))*expC)/(1.0+u*u);
     }
-    template<typename Number, typename Index>
-    auto phiK(const Number& a, const Number& b, const Number& c, const Number& d, const Index& k){
+    template<typename Number, typename U>
+    auto phiK(const Number& a, const Number& b, const Number& c, const Number& d, const U& u){
    
-        auto kPiBA=k*M_PI/(b-a);
+        //auto u=k*M_PI/(b-a);
         auto iterS=[&](const auto& x){
-            return kPiBA*(x-a);
+            return u*(x-a);
         };
-        return k==0?d-c:(sin(iterS(d))-sin(iterS(c)))/kPiBA;
+        return u==0.0?d-c:(sin(iterS(d))-sin(iterS(c)))/u;
     }
     
     /**
-    For a call, the "VK" is (2/(b-a))*K(chiK(0, b)-phiK(0, b))
+    Note that this is a put!  It has better stability and put call parity can be used to get the call back
     */
     template<typename Index, typename Number,  typename CF>
     auto FangOost(
         const Index& numXSteps, 
+        //const KArray& K, 
         const Index& numUSteps, 
         const Number& xMin, 
-        const Number& xMax, 
-        const Number& K, 
+        const Number& xMax,      
+        const Number& K,      
         const Number& discount,
         CF&& cf
      ){
-    
-        //int xDiscrete, int uDiscrete,  const Number& xMin, const Number& xMax, auto&& fnInv, auto&& vK
-        return fangoost::computeInv(numXSteps, numUSteps, xMin, xMax, cf, 
-           /* [&](const auto& u, const auto& x){
-                return K*(chiK(xMin, xMax, 0.0, xMax, index)-phiK(xMin, xMax, 0.0, xMax, index))*2.0/(xMax-xMin);
-            });*/
-        
-        
-        futilities::for_each_parallel(0, numUSteps, [&](const auto& index){
-            return K*(chiK(xMin, xMax, 0.0, xMax, index)-phiK(xMin, xMax, 0.0, xMax, index))*2.0/(xMax-xMin);
-        }));
-        
+        return fangoost::computeExpectation(numXSteps, numUSteps, xMin, xMax, cf, [&](const auto& u, const auto& x){
+            return discount*K*(phiK(xMin, xMax, xMin, 0.0, u)-chiK(xMin, xMax, xMin, 0.0, u));
+        });
     }
     template<typename Index, typename Number,  typename CF>
     auto FangOost(
         const Index& numXSteps, 
         const Index& numUSteps, 
-        const Number& xMax, 
-        const Number& K, 
+        const Number& xMax,     
+        const Number& K,      
         const Number& discount,
         CF&& cf
      ){
@@ -144,14 +184,7 @@ namespace optionprice{
         //auto u=;
         return cf(v+(alpha+1.0))/(alpha*alpha+alpha+v*v+(2*alpha+1)*v);
     }
-    template<typename Number>
-    auto getMaxK(const Number& ada){
-        return M_PI/ada;
-    }
-    template<typename Index, typename Number>
-    auto getLambda(const Index& numSteps, const Number& b){
-        return 2.0*b/numSteps;
-    }
+    
     /**
     Carr-Madan: with respsect to K, not S
 
@@ -161,6 +194,7 @@ namespace optionprice{
         const Index& numSteps, 
         const Number& ada, 
         const Number& alpha, 
+        const Number& S0, 
         const Number& discount, 
         CF&& augCF
     ){
@@ -176,18 +210,19 @@ namespace optionprice{
             )
         );
         return futilities::for_each_parallel(0, numSteps, [&](const auto& index){
-            return cmplVector[index].real()*exp(-alpha*getDomain(-b, lambda, index))*ada/(M_PI*3.0);
+            return S0*cmplVector[index].real()*exp(-alpha*getDomain(-b, lambda, index))*ada/(M_PI*3.0);
         });
     }
     template<typename Index, typename Number,  typename CF>
     auto CarrMadan(
         const Index& numSteps, 
         const Number& ada, 
+        const Number& S0,
         const Number& discount, 
         CF&& augCF
     ){
         auto alpha=1.5;
-        return CarrMadan(numSteps, ada, alpha, discount, augCF);
+        return CarrMadan(numSteps, ada, alpha, S0, discount, augCF);
     }
 }
 
