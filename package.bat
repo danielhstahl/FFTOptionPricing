768364eea5b8e0bc7b59e36875cf04534e6e8c05
Daniel Stahl <danstahl1138@gmail.com>
2016-12-27 07:43:39 -0600

    update bat

--- package.bat
+++ package.bat
@@ -7,7 +7,7 @@ CALL :cloneAndCheckout FunctionalUtilities cb634c343585267d51409a4af1871dde613f8
 CALL :cloneAndCheckout CharacteristicFunctions 59d5f2e1789bdcfc8d861645ef3d6392de79832f
 CALL :cloneAndCheckout FangOost d74b4e541aa5569ccd99192fe3d256e7d23b3883
 CALL :cloneAndCheckout RungeKutta b286ce51f5e6957a59a5a49a3d1c60abdd765af4
-
+EXIT /B %ERRORLEVEL%
 
 :cloneAndCheckout 
 ECHO %1
