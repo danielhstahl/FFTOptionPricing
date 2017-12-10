b1535babb793926d8a5219c45072d130e628b987
Daniel Stahl <danstahl1138@gmail.com>
2017-12-10 10:30:00 -0600

    fixed tests and ensured reducing to BS still works

--- test.cpp
+++ test.cpp
@@ -200,6 +200,50 @@ TEST_CASE("FangOosterleeCallFewK", "[OptionPricing]"){
         REQUIRE(myOptionsPrice[i]==Approx(analyticOption).epsilon(.0001));
     }
 }
+TEST_CASE("FangOosterleeCallFewKLowT", "[OptionPricing]"){
+    auto r=.05;
+    auto sig=.3;
+    auto T=.25;
+    auto S0=50.0; 
+   
+
+    auto BSCF=[&](const auto& u){
+        //return chfunctions::gaussCF(u, (r-sig*sig*.5)*T, sig*sqrt(T));
+        return exp(chfunctions::gaussLogCF(u, r-sig*sig*.5, sig)*T);
+    };
+    auto discount=exp(-r*T);
+
+    auto BS=[&](const auto &S0, const auto &discount, const auto &k, const auto &sigma){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
+        if(sigma>0){
+            double s=sqrt(2.0);
+            auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
+            return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma)/s)));
+        }
+        else{
+            if(S0>k){
+                return (S0-k)*discount;
+            }
+            else{
+                return 0.0;
+            }
+        } 
+    };
+    int numU=256; //This should run extremely quickly since only have 5 strikes, the higher U is to compensate for low t
+    auto KArray=std::vector<double>(5, 0);
+    KArray[4]=.3;
+    KArray[3]=40;//strike 40
+    KArray[2]=50;//strike 50
+    KArray[1]=60;//strike 60
+    KArray[0]=7500;
+    auto started = std::chrono::high_resolution_clock::now();
+    auto myOptionsPrice=optionprice::FangOostCall(S0, KArray, numU, discount, BSCF);
+    auto done = std::chrono::high_resolution_clock::now();
+    std::cout << "Fang Oosterlee time: "<<std::chrono::duration_cast<std::chrono::milliseconds>(done-started).count()<<std::endl;
+    for(int i=1;i<4; ++i){ 
+        auto analyticOption=BS(S0, discount, KArray[i], sig*sqrt(T));
+        REQUIRE(myOptionsPrice[i]==Approx(analyticOption).epsilon(.0001));
+    }
+}
 
 
 
@@ -254,6 +298,50 @@ TEST_CASE("CarrMadanCall", "[OptionPricing]"){
     auto r=.05;
     auto sig=.3;
     auto T=1.0;
+    auto S0=50.0;
+    auto discount=exp(-r*T);
+    auto BSCF=[&](const auto& u){
+        return chfunctions::gaussCF(u, (r-sig*sig*.5)*T, sig*sqrt(T));
+    };
+    auto BS=[&](const auto &S0, const auto &discount, const auto &k, const auto &sigma){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
+        if(sigma>0){
+            double s=sqrt(2.0);
+            auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
+            return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma)/s)));
+        }
+        else{
+            if(S0>k){
+                return (S0-k)*discount;
+            }
+            else{
+                return 0.0;
+            }
+        }
+    };
+    int numX=pow(2, 10);
+    auto ada=.25;
+    auto started = std::chrono::high_resolution_clock::now();
+    auto myOptionsPrice=optionprice::CarrMadanCall(numX,  
+        ada,
+        S0, 
+        discount, 
+        BSCF
+    );
+    auto done = std::chrono::high_resolution_clock::now();
+    std::cout << "Carr Madan time: "<<std::chrono::duration_cast<std::chrono::milliseconds>(done-started).count()<<std::endl;
+    auto b=optionprice::getMaxK(ada);
+    auto lambda=optionprice::getLambda(numX, b);
+    auto myXDomain=optionprice::getCarrMadanStrikes(ada, S0, numX);
+    int i=(int)numX*.3;
+    int mxX=(int)numX*.7;
+    for(;i<mxX; ++i){
+        REQUIRE(myOptionsPrice[i]==Approx(BS(S0, discount, myXDomain[i], sig*sqrt(T))).epsilon(.0001));
+    }
+}
+TEST_CASE("CarrMadanCallLowT", "[OptionPricing]"){
+    auto r=.05;
+    auto sig=.3;
+    auto T=.25;
     auto S0=50.0;
     auto discount=exp(-r*T);
     auto BSCF=[&](const auto& u){
@@ -349,7 +437,7 @@ TEST_CASE("FangOosterleeCallCGMY", "[OptionPricing]"){
     //90 98 0.25 0.06 0.0 0.0 16.97 7.08 29.97 0.6442
     auto KArray=std::vector<double>(3, 0);
     KArray[2]=.3;
-    KArray[1]=98;//strike 40
+    KArray[1]=98;//strike 98
     KArray[0]=7500;
 
     auto r=.06;
@@ -371,5 +459,152 @@ TEST_CASE("FangOosterleeCallCGMY", "[OptionPricing]"){
     auto myReference=16.212478;//https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 19
     REQUIRE(myOptionsPrice[1]==Approx(myReference).epsilon(.0001));
 
+
+}
+TEST_CASE("FangOosterleeCallCGMYWitht=1", "[OptionPricing]"){
+    //http://ta.twi.tudelft.nl/mf/users/oosterle/oosterlee/COS.pdf pg 19
+    //S0 = 100, K = 100, r = 0.1, q = 0, C = 1, G = 5, M = 5, T = 1
+    //= 19.812948843
+    auto KArray=std::vector<double>(3, 0);
+    KArray[2]=.3;
+    KArray[1]=100.0;//strike 98
+    KArray[0]=7500;
+
+    auto r=.1;
+    auto sig=0.0;
+    auto T=1.0;
+    auto S0=100.0;  
+    auto C=1.0;
+    auto G=5.0;
+    auto M=5.0;
+    auto Y=0.5;
+    auto discount=exp(-r*T);
+
+    auto BSCF=[&](const auto& u){
+        return exp(chfunctions::cgmyLogRNCF(u, C, G, M, Y, r, sig)*T);
+    };
+    
+    int numU=64; //
+    auto myOptionsPrice=optionprice::FangOostCall(S0, KArray, numU, discount, BSCF);
+    auto myReference=19.812948843;
+    REQUIRE(myOptionsPrice[1]==Approx(myReference).epsilon(.0001));
+
+
+}
+
+
+TEST_CASE("FangOosterleeCallCGMYReducesToBS", "[OptionPricing]"){
+    //https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 20
+    //S K T r q σ C G M Y
+    //500 500 0.25 0.4 0.0 0.2 1.0 1.4 2.5 1.4
+    auto KArray=std::vector<double>(3, 0);
+    KArray[2]=.3;
+    KArray[1]=500.0;//strike 500.0
+    KArray[0]=7500;
+
+    auto r=.4;//seems high
+    auto sig=0.2;
+    auto T=.25;
+    auto S0=500.0;  
+    auto C=0.0; //THIS IS ZERO, to reduce to bS
+    auto G=1.4;
+    auto M=2.5;
+    auto Y=1.4;
+    auto discount=exp(-r*T);
+
+    auto BSCF=[&](const auto& u){
+        return exp(chfunctions::cgmyLogRNCF(u, C, G, M, Y, r, sig)*T);
+    };
+    auto BS=[&](const auto &S0, const auto &discount, const auto &k, const auto &sigma){ //note that sigma includes sqrt(t) term so in vanilla BS sigma is equal to volatility*sqrt(T)
+        if(sigma>0){
+            double s=sqrt(2.0);
+            auto d1=log(S0/(discount*k))/(sigma)+sigma*.5;
+            return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma)/s)));
+        }
+        else{
+            if(S0>k){
+                return (S0-k)*discount;
+            }
+            else{
+                return 0.0;
+            }
+        }
+    };
+    
+    int numU=256; //higher due to low T
+    auto myOptionsPrice=optionprice::FangOostCall(S0, KArray, numU, discount, BSCF);
+    auto myReference=BS(S0, discount, KArray[1], sig*sqrt(T));
+    REQUIRE(myOptionsPrice[1]==Approx(myReference).epsilon(.0001));
+
+
+}
+
+
+TEST_CASE("FangOosterleeCallCGMYWithVol", "[OptionPricing]"){
+    //https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 20
+    //S K T r q σ C G M Y
+    //500 500 0.25 0.4 0.0 0.2 1.0 1.4 2.5 1.4
+    auto KArray=std::vector<double>(3, 0);
+    KArray[2]=.3;
+    KArray[1]=500.0;//strike 500.0
+    KArray[0]=7500;
+
+    auto r=.4;//seems high
+    auto sig=0.2;
+    auto T=.25;
+    auto S0=500.0;  
+    auto C=1.0;
+    auto G=1.4;
+    auto M=2.5;
+    auto Y=1.4;
+    auto discount=exp(-r*T);
+
+    auto BSCF=[&](const auto& u){
+        return exp(chfunctions::cgmyLogRNCF(u, C, G, M, Y, r, sig)*T);
+    };
+    
+    int numU=256; //this is high...this seems to be a computationally tricky problem
+    auto myOptionsPrice=optionprice::FangOostPut(S0, KArray, numU, discount, BSCF);
+    //auto myReference=108.49914;//https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 20
+    auto myReference=108.445317144;//This is what I get when I keep increasing U
+    REQUIRE(myOptionsPrice[1]==Approx(myReference).epsilon(.0001));
+
+
+}
+
+TEST_CASE("CarrMadanCGMY", "[OptionPricing]"){
+    //https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 20
+    //S K T r q σ C G M Y
+    //500 500 0.25 0.4 0.0 0.2 1.0 1.4 2.5 1.4
+    auto r=.4;//seems high
+    auto sig=0.2;
+    auto T=.25;
+    auto S0=500.0;  
+    auto C=1.0;
+    auto G=1.4;
+    auto M=2.5;
+    auto Y=1.4;
+    auto discount=exp(-r*T);
+
+    auto BSCF=[&](const auto& u){
+        return exp(chfunctions::cgmyLogRNCF(u, C, G, M, Y, r, sig)*T);
+    };
+    
+    
+    int numX=pow(2, 10); //this is high...this seems to be a computationally tricky problem
+    auto ada=.25;
+    auto myOptionsPrice=optionprice::CarrMadanPut(numX,  
+        ada,
+        S0, 
+        discount, 
+        BSCF
+    );
+   
+    auto b=optionprice::getMaxK(ada);
+    auto lambda=optionprice::getLambda(numX, b);
+    auto myXDomain=optionprice::getCarrMadanStrikes(ada, S0, numX);
+    //auto myReference=108.49914;//https://cs.uwaterloo.ca/~paforsyt/levy.pdf pg 20
+    auto myReference=108.4614527579;//This is what I get when I keep increasing U 
+    REQUIRE(myOptionsPrice[numX/2]==Approx(myReference).epsilon(.0001));
 
 }
